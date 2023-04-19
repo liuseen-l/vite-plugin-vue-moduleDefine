8e02fc8fd8348b874c08776246b95b55c6fd1744
code-ManL <773890440@qq.com>
2023-04-20 01:01:19 +0800

    feat: is hook

--- tests/fixtures/index.ts
+++ tests/fixtures/index.ts
@@ -1,20 +1,47 @@
 import { walk } from 'estree-walker'
+import type { Node } from 'acorn'
 import { parse } from 'acorn'
+import type { CallExpression, Identifier } from '@babel/types'
 
-const script = `
-const a =1
-const b =2
-
+const script
+  = `const props = defineProps({
+  foo:String
+})
 `
+const nodeList: Node[] = []
+
+function isCallExpression(node: any): node is CallExpression {
+  return node.type && node.type === 'CallExpression'
+}
+
+function isIdentifier(node: any): node is Identifier {
+  return node.type && node.type === 'Identifier'
+}
+
+function isMacro(node: Node): boolean {
+  if (isCallExpression(node)) {
+    if (isIdentifier(node.callee) && node.callee.name)
+      nodeList.push(node)
+    return true
+  }
+  return false
+}
+
 const ast = parse(script, { ecmaVersion: 'latest', sourceType: 'module' })
 
   ; (walk as any)(ast, {
   enter(node: Node, parent?: Node) {
     // some code happens
-    // console.log('enter', node, parent)
+    // if (node.type && node.type === 'CallExpression' && node.callee.name === 'defineProps')
+    const r = isMacro(node)
+    if (r)
+      this.remove()
+    // console.log('enter', node)
   },
   leave(node: Node, parent?: Node) {
     // some code happens
-    // console.log('leave', node, parent)
+    // console.log('leave', node)
   },
 })
+
+console.log(nodeList)

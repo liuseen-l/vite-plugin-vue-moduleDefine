6faa24272fb79922b869d5f97caf784353defff3
code-ManL <773890440@qq.com>
2023-04-19 10:56:13 +0800

    chore: process module

--- core/index.ts
+++ core/index.ts
@@ -5,18 +5,25 @@ import { parse as _parse, compileScript } from '@vue/compiler-sfc'
 import fs from 'node:fs/promises'
 import path from 'node:path'
 
-
 // 处理 import 
 async function processSetupImports(source: string, id: string): Promise<string[]> {
   const { descriptor } = _parse(source)
   const imports = compileScript(descriptor, { id: 'v' }).imports as object
-  const matchImports = Object.keys(imports).filter(key =>
-    imports[key].isFromSetup && key.startsWith('.'))
+  const matchImports = Object.fromEntries(Object.entries(imports).filter(([key]) =>
+    (imports[key].isFromSetup && imports[key].source.startsWith('.'))
+  ))
 
-  for (const match of matchImports) {
-    await fs.readFile(path.resolve(__dirname, id), 'utf-8')
+  const reg = /defineProps\(([.\b\s\S]*)\)/
+  const markable: string[] = []
+
+  for (const key of Object.keys(matchImports)) {
+    const content = await fs.readFile(path.resolve(id, imports[key].source), 'utf-8')
+    if (content.match(reg)) {
+      // compile the match file from .vue
+      compile(content)
+    }
   }
-  return ['da']
+  return markable
 }
 
 function generator(ast): string {
@@ -24,7 +31,7 @@ function generator(ast): string {
   return code
 }
 
-function parser(source: string) {
+function parse(source: string) {
   const ast = baseParse(source)
     ; (walk as any)(ast, {
       enter(node: Node, parent?: Node) {
@@ -41,7 +48,7 @@ function parser(source: string) {
 
 
 function compile(source: string) {
-  const ast = parser(source)
+  const ast = parse(source)
 
 }
 
@@ -52,10 +59,10 @@ export default () => {
   return {
     name: "vite-plugin-vue-moduleDefine",
     enforce: "pre",
-    transform(code: string, id: string) {
+    async transform(code: string, id: string) {
       if (id.endsWith('vue')) {
         // 拿到当前 vue 文件 setup 中导入的所有的ts文件
-        const res = processSetupImports(code, id)
+        const matchList = await processSetupImports(code, id)
 
         // 循环遍历
 
